a213a9de34102b13f8fc6ca01124ff5cfd164e8f
Toby Baek <50404311+pigako@users.noreply.github.com>
2021-06-08 09:46:17 +0900

    Add .env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
+import * as Joi from "joi";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development",
+            validationSchema: Joi.object({
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+                DB_CHARSET: Joi.string().required(),
+                DB_TIMEZONE: Joi.string().required()
+            })
+        })
+    ]
 })
 export class AppModule {}
