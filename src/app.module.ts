68a64115b0b52ff787ffa22429f82dc6a7c1fd59
Toby Baek <50404311+pigako@users.noreply.github.com>
2021-06-08 10:20:37 +0900

    Connect Database with tpyeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import * as Joi from "joi";
 @Module({
@@ -16,6 +17,19 @@ import * as Joi from "joi";
                 DB_CHARSET: Joi.string().required(),
                 DB_TIMEZONE: Joi.string().required()
             })
+        }),
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            charset: process.env.DB_CHARSET,
+            timezone: process.env.DB_TIMEZONE,
+            synchronize: true,
+            logging: true,
+            entities: [__dirname + "/entities/*{.ts,.js}"]
         })
     ]
 })
